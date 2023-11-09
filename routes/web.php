5559094106906d44ccc01704f926d001f23cad73
Josadad <ernestcribano@hotmail.com>
2023-11-09 13:47:25 -0600

    Se agregan el resto de las rutas en el documento web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\OfflineController;
+use App\Http\Controllers\OnlineController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+
+
 
 
 /*
@@ -34,5 +40,19 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     //Post
     Route::post('/create-post', [PostController::class, 'createPost'])->name('create-post');
     Route::get('/list-posts', [PostController::class, 'getPosts'])->name('list-post');
-    Route::get('/like-post', [PostController::class, 'likeOrDislike'])->name('like-post');
+    Route::post('/like-post', [PostController::class, 'likeOrDislike'])->name('like-post');
+    Route::post('/comment', [PostController::class, 'comment'])->name('comment');
+
+    //profile
+    Route::get('/profile/{nick_name}', [ProfileController::class, 'index'])->name('profile');
+
+    //offline and online
+    Route::post('/online/{id}', OnlineController::class)->name('online');
+    Route::post('/offline/{id}', OfflineController::class)->name('offline');
+
+    //follow
+    Route::post('/follow-user', [ProfileController::class, 'followUser'])->name('follow-user');
+    Route::post('/unfollow-user', [ProfileController::class, 'unFollow'])->name('unfollow-user');
+    Route::get('/exists-follow/{user_id}', [ProfileController::class, 'existsFollow'])->name('exists-follow');
+    Route::post('/markAsRead', [ProfileController::class, 'markAsRead'])->name('markAsRead');
 });
